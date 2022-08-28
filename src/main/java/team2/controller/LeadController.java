c5b11ffc1f01c52b3754c4788c0da13283d83f84
santhoshgit8 <75970980+santhoshgit8@users.noreply.github.com>
2022-08-28 22:04:15 +0530

    Update LeadController.java

--- src/main/java/team2/controller/LeadController.java
+++ src/main/java/team2/controller/LeadController.java
@@ -5,8 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
-@SpringBootApplication
+@Controller
 public class LeadController {
 	
 	@RequestMapping("/")
